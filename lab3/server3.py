0e18a131478b958c6fa7c8764468580e8828967e
stereopolina <64332054+stereopolina@users.noreply.github.com>
2021-08-31 22:19:58 +0300

    Update server3.py

--- lab3/server3.py
+++ lab3/server3.py
@@ -4,31 +4,26 @@ PORT = 65435
 BUF_SIZE = 1024
 from socket import socket, AF_INET, SOCK_DGRAM, timeout
 import time
+IMAGES = dict()
+seqno_0 = 0
+seqno = 0
 
-
-
-def converter(num):
-    if num*10%10 == 0:
-        return int(num)
-    return num
-
-
-def divider(data):
-    str = data.decode()
-    expression = str.split(" ")
-    sign = expression[0]
-    a = expression[1]
-    b = expression[2]
-    return sign, a, b
-
-def type_converter(num1):
-    if num1.find('.') != -1:
-        num1 = float(num1)
-    else:
-        num1 = int(num1)
-    return num1
-
-
+def get_file(s):
+    seqno = 1
+    flag = True
+    while flag:
+        data, addr = s.recvfrom(BUF_SIZE + 50)
+        command, seqno, data_bytes = data.decode().split("|")
+        seqno = int(seqno)
+        if command == 'd':
+            IMAGES[seqno] = data_bytes
+            seqno += 1
+            respond = f"a|{seqno}|{BUF_SIZE}"
+            s.sendto(respond.encode(), addr)
+        if (flag == False):
+            print("The server has quit")
+            flag = False
+            break
 
 if __name__ == "__main__":
     with socket(AF_INET, SOCK_DGRAM) as s:
@@ -38,23 +33,18 @@ if __name__ == "__main__":
         try:
             while True:
                 print("Waiting for a new file")
-                try:
-                    data, addr = s.recvfrom(BUF_SIZE)
-                    command, seqno, extension, size = data.decode().split(" | ")
-                    if command =="s":
-                        # time.sleep(8)
-                        respond = "ack"
-                        print(respond)
-                        s.sendto(respond.encode(), addr)
+                data, addr = s.recvfrom(BUF_SIZE + 50)
+                command, seqno, extension, size = data.decode().split("|")
+                if command =="s":
+                    # time.sleep(8)
+                    respond = f"a|{seqno}|{BUF_SIZE}"
+                    s.sendto(respond.encode(), addr)
+                    get_file(s)
 
-                except KeyError:
-                    print('OOps')
-                #
-                # except ValueError:
-                #
-                #
-                # except ZeroDivisionError:
+
 
 
         except KeyboardInterrupt:
             print(f"Server was stopped at {time.time()}")
+
+
